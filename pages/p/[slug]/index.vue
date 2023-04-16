1018fa010b4e57efed2d69d6d8453ffa34d9dc88
noruwa03 <noruwaobaseki@gmail.com>
2023-04-16 23:38:35 +0100

    Added blogHTML ql-syntax to tailwindcss input

--- pages/p/[slug]/index.vue
+++ pages/p/[slug]/index.vue
@@ -91,7 +91,7 @@ fetchData();
               dataStyle: "long",
             })
           }}</span>
-          <div v-html="postDetail.content[0].blogHTML" class="mt-8 html-list-item"></div>
+          <div v-html="postDetail.content[0].blogHTML" class="mt-8 blogHTML"></div>
         </section>
       </div>
     </div>
